e288c4f5a012d80e4e6ab6beaa27e21383b0bcc9
Renato Groffe <renato.groffe@yahoo.com.br>
2021-08-06 15:52:23 -0300

    .NET 6 Preview 6 + ASP.NET Core + Razor Pages + Docker + Linux
    .NET 6 Preview 6 + ASP.NET Core + Razor Pages + Docker + Linux

---
+++ SiteCanaisTecnologia/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SiteCanaisTecnologia.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation(
+                $"Acesso ao site em: {DateTime.UtcNow.AddHours(-3):dd/MM/yyyy HH:mm:ss}");
+            TempData["Saudacao"] = _configuration["Saudacao"];
+        }
+    }
+}
